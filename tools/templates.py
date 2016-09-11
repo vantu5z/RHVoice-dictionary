75d9c36d2ae9a71edb52c8b4407d37610b3b6331
vantu5z <vantu5z@mail.ru>
2016-09-11 21:47:12 +0600

    добавил 'ё' для Петра и Фёдора и пополнил тест

--- tools/templates.py
+++ tools/templates.py
@@ -1007,7 +1007,7 @@ def txt_prep(text):
         text = text.replace(m.group(), ordinal(m.group(1), i_sr), 1)
 
     # Склонение порядковых числительных при именах собственных
-    for m in finditer(r'(Александр|Иван|Иоанн|Пав[е]?л|Петр|Федор|Васили|Лжедмитри|Никола)' + '(|а|е|ем|й|ом|у|ю|я)' + r' (\d+)', text):
+    for m in finditer(r'(Александр|Иван|Иоанн|Пав[е]?л|П[её]тр|Ф[её]дор|Васили|Лжедмитри|Никола)' + '(|а|е|ем|й|ом|у|ю|я)' + r' (\d+)', text):
         text = text.replace(m.group(), m.group(1) + m.group(2) + ' ' + ordinal(m.group(3), mu_pd[m.group(2)]), 1)
     for m in finditer(r'(Анн|Екатерин)' + '(|а|е|ой|у|ы)' + r' (\d+)', text):
         text = text.replace(m.group(), m.group(1) + m.group(2) + ' ' + ordinal(m.group(3), zh_pd[m.group(2)]), 1)
