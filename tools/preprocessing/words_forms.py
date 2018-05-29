0ed51305ff3db39b7fec8b0e5ea8384b2aefadeb
vantu5z <vantu5z@mail.ru>
2018-05-29 15:19:50 +0300

    подключение pymorphy2 для определения формы слов если модуль не установлен, то работаем со встроенным словарем

--- tools/preprocessing/words_forms.py
+++ tools/preprocessing/words_forms.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# импорт pymorphy2 для определения атрибутов слов
+try:
+    import pymorphy2
+    morph = pymorphy2.MorphAnalyzer()
+    is_morph = True
+except:
+    print('Не установлен "pymorphy2".'
+          'Определение аттрибутов слова будет вестись по словарю.')
+    is_morph = False
+
 # импорт словарей
 import words_muz
 import words_zen
@@ -10,6 +20,7 @@ import words_sre
 M_GENDER = 'мужской'
 Z_GENDER = 'женский'
 S_GENDER = 'средний'
+O_GENDER = 'общий'
 
 
 class Words():
@@ -20,15 +31,67 @@ class Words():
         """
         Инициализация.
         """
+        # инициализация словарей
         self.muz = WordsForms(words_muz.words, M_GENDER)
         self.zen = WordsForms(words_zen.words, Z_GENDER)
         self.sre = WordsForms(words_sre.words, S_GENDER)
+
+        # поддержка pymorphy2
+        if is_morph:
+            self.morph = morph
+        else:
+            self.morph = None
+
+    def parse_morph(self, records):
+        """
+        Приведение результатов разобранного слова с помощью pymorphy2
+        к нужному виду.
+        Возвращает: <род>, <число>, (<список совпавших падежей>)
+        """
+        # первый разбор слова
+        first_tag = records[0].tag.cyr_repr
+
+        # число
+        plural = 'мн' in first_tag
+
+        # род
+        if 'ор' in first_tag: gender = O_GENDER
+        elif 'мр' in first_tag: gender = M_GENDER
+        elif 'жр' in first_tag: gender = Z_GENDER
+        elif 'ср' in first_tag: gender = S_GENDER
+        else:
+            return False, None, None, None
+
+        # совпадение падежей
+        case = [0, 0, 0, 0, 0, 0]
+        for item in records:
+            tag = item.tag.cyr_repr
+            if ('СУЩ' in tag):
+                if 'им' in tag: case[0] = 1
+                if 'рд' in tag: case[1] = 1
+                if 'дт' in tag: case[2] = 1
+                if 'вн' in tag: case[3] = 1
+                if 'тв' in tag: case[4] = 1
+                if 'пр' in tag: case[5] = 1
+
+        if 1 in case:
+            return True, gender, plural, case
+        else:
+            return False, None, None, None
 
     def get_attr(self, word):
         """
         Определение аттрибутов слова.
         Возвращает: <род>, <число>, (<список совпавших падежей>)
         """
+        # если подключен pymorphy2
+        if self.morph:
+            result = self.morph.parse(word)
+            found, gender, plural, case = self.parse_morph(result)
+            print(word, found, gender, plural, case)
+            return found, gender, plural, case
+
+        # поиск слова по словарю
         found, gender, plural, case = self.muz.get_attr(word)
         if not found:
             found, gender, plural, case = self.zen.get_attr(word)
