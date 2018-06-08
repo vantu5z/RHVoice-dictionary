9bdd5d985856c71965396049fe7cfa390a835666
vantu5z <vantu5z@mail.ru>
2018-06-08 13:02:15 +0300

    дополнительные проверки + комментарии

--- tools/preprocessing/text_prepare.py
+++ tools/preprocessing/text_prepare.py
@@ -23,10 +23,13 @@ def text_prepare(text):
     =================================
     """
 
+    # предварительная обработка текста
     for sample in presamples:
         text = sub(sample[0], sample[1], text)
 
+    # =================
     # Единицы измерения
+    # =================
 
     # Винительный падеж
     # например: "диаметром в 2 см -> диаметром в 2 сантиметра"
@@ -62,16 +65,22 @@ def text_prepare(text):
         text = text.replace(m.group(), m.group(1) + ' в ' + number, 1)
 
     # Родительный падеж
+    # пример: "С 5 см до -> С пяти сантиметров до"
     for m in finditer(r'\b([Сс] (почти |примерно |приблизительно |плюс |минус |))(\d+)_' + units + ' до ', text):
-        text = text.replace(m.group(), m.group(1) + m.group(3) + ' ' + substant(m.group(3), m.group(4), 1) + ' до ', 1)
+        new = m.group(1) + m.group(3) + ' ' + substant(m.group(3), m.group(4), 1) + ' до '
+        text = text.replace(m.group(), new, 1)
+
+    # пример: "от 1 до 4 км -> от одного до четырёх километров"
     for m in finditer(r'\b([Оо]т |[Сс]о? )(\d+,|)(\d+)( до (\d+,|)\d+_)' + units, text):
-        if m.group(2) != '':
+        if m.group(2):
             number = fraction(m.group(2)[:-1], m.group(3), 1)
         else:
             number = cardinal(m.group(3), r_ca)
             if m.group(6) in zh_units:
                 number = number[:-2] + 'й'
-        text = text.replace(m.group(), m.group(1) + number + m.group(4) + m.group(6), 1)
+        new = m.group(1) + number + m.group(4) + m.group(6)
+        text = text.replace(m.group(), new, 1)
+
     for m in finditer(r'\b([Бб]олее|[Мм]енее|[Бб]ольше|[Мм]еньше|[Вв]ыше|[Нн]иже|[Оо]коло|[Сс]выше|[Дд]ля|[Дд]о|[Ии]з|[Оо]т|[Вв]место|[Вв] размере|[Вв] течение|[Нн]ач[инаетсялоь]{2,7} с|[Вв]ладел[аеимухцыь]{2,5}|[Дд]остиг[авеийлнотшщюуья]{,5}|[Пп]ротив|[Пп]орядка|[Пп]осле)( плюс | минус | )((\d+,|)(\d+)( - | или | и )(плюс |минус |)|)(\d+,|)(\d+)_' + units, text):
         if m.group(3):
             if m.group(4):
