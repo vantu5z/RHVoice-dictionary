aa170c438b2c07db053e6ad706884ec82eae75eb
capricorn2001 <capricorn2001@bigmir.net>
2018-09-14 10:44:27 +0300

    14.09.2018

--- tools/preprocessing/text_prepare.py
+++ tools/preprocessing/text_prepare.py
@@ -351,9 +351,14 @@ def text_prepare(text):
 
     mask = (r'\b(?<!,)(\d*[02-9][05-9]|\d*1\d|[5-9]) ([а-яё]+)\b')
     for m in finditer(mask, text):
+        number = ''
         attr = words.get_attr(m.group(2))
         if  attr.have([M_GENDER, S_GENDER], False, [1]):
-            new = ordinal(m.group(1), r_mu) + ' ' + m.group(2)
+            number = ordinal(m.group(1), r_mu)
+        if  attr.have([Z_GENDER], False, [1]):
+            number = ordinal(m.group(1), r_zh)
+        if number:
+            new = number + ' ' + m.group(2)
             text = text.replace(m.group(), new, 1)
 
     for pattern in patterns:
