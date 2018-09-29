379a2dec99101991e0c3d6999b2f1e8eb0acf29e
capricorn2001 <capricorn2001@bigmir.net>
2018-09-29 21:27:25 +0300

    29.09.2018-3

--- tools/preprocessing/text_prepare.py
+++ tools/preprocessing/text_prepare.py
@@ -660,7 +660,9 @@ def text_prepare(text):
             pre = cardinal(m.group(3), v_ca)
             if pre[-3:] == 'дин':
                 attr = words.get_attr(m.group(9))
-                if attr.have([Z_GENDER], False, [3]):
+                if attr.have([M_GENDER], False, [3]):
+                    pre = pre[:-2] + 'ного'
+                elif attr.have([Z_GENDER], False, [3]):
                     pre = pre[:-2] + 'ну'
                 elif attr.have([S_GENDER], False, [0, 3]):
                     pre = pre[:-2] + 'но'
@@ -670,7 +672,9 @@ def text_prepare(text):
         number = cardinal(m.group(5), v_ca)
         if number[-3:] == 'дин':
             attr = words.get_attr(m.group(9))
-            if attr.have([Z_GENDER], False, [3]):
+            if attr.have([M_GENDER], False, [3]):
+                number = number[:-2] + 'ного'
+            elif attr.have([Z_GENDER], False, [3]):
                 number = number[:-2] + 'ну'
             elif attr.have([S_GENDER], False, [0, 3]):
                 number = number[:-2] + 'но'
