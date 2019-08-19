3a5c195bc9599df2a372dfe32e08a6b632a2d484
vantu5z <vantu5z@mail.ru>
2019-08-19 11:04:24 +0300

    Исправлено: 220 вольт "220V" -> "200 двадцатого вольт"

--- tools/preprocessing/rules.py
+++ tools/preprocessing/rules.py
@@ -650,7 +650,8 @@ class CountRule_9(RuleBase):
         number = ''
         attr = words.get_attr(m.group(3))
         if attr.have([M_GENDER, S_GENDER], False, [1]):
-            number = ordinal(m.group(2), r_mu)
+            if not attr.have(case=[0]):
+                number = ordinal(m.group(2), r_mu)
         if attr.have([Z_GENDER], False, [1]):
             number = ordinal(m.group(2), r_zh)
         if number:
