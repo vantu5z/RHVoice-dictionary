18d44b7f19cc0f9c121b43d62866a80c8201de19
capricorn2001 <capricorn2001@bigmir.net>
2019-11-16 16:51:23 +0300

    16.11.2019-3

--- tools/preprocessing/templates.py
+++ tools/preprocessing/templates.py
@@ -1450,6 +1450,7 @@ samples_3 = (
   (r'(\d+)(|-й) (или|и|-) (\d+)(|-й) (годы|гг\.)',
    'ordinal(m.group(1), i_mu) + " " + m.group(3) + " " + '
    'ordinal(m.group(4), i_mu) + " годы"'),
+  (r'\b(\d+)-й(?= годы)\b', 'ordinal(m.group(1), i_mu)'),
   (r'(\d+)-е' + months, 'ordinal(m.group(1), i_sr) + " " + m.group(2)'),
   (r'([Мм]ежду )(\d+)' + months + '( и )(\d+)' + months,
    'm.group(1) + ordinal(m.group(2), t_mu) + " " + m.group(3) + '
