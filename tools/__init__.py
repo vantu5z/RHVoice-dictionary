6f0ba540bca8f4cf2babac4cd38c955996bd266a
vantu5z <vantu5z@mail.ru>
2021-01-15 11:57:30 +0300

    исправил название каталогов

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from .preprocessing.text_prepare import text_prepare
-from .rhvoice-say.rhvoice_say import rhvoice_say, say_clipboard
-from .rhvoice-config.rhvoice_conf_gui import main as rhvoice_config
+from .rhvoice_say.rhvoice_say import rhvoice_say, say_clipboard
+from .rhvoice_config.rhvoice_conf_gui import main as rhvoice_config
